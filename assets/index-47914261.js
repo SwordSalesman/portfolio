6bfb2158a961caa3325aa37ce7cceee98e96a233
Luke Cannon <lewk.canyon@gmail.com>
2023-04-21 08:50:34 +0800

    Updates

--- assets/index-47914261.js
+++ assets/index-47914261.js
@@ -350,11 +350,15 @@ After:   <div {...getCollapseProps()}>
   max-height: 20rem;
   height: 100%;
 
-  box-shadow: 0 0.3rem 0.6rem rgba(0, 0, 0, 0.15);
+  box-shadow: 0 0.3rem 0.6rem rgba(0, 0, 0, 0.35);
 
-  &:hover {
-    transform: scale(1.3);
-    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.35);
+  @media (min-width: 600px) {
+    max-width: 500px;
+
+    &:hover {
+      transform: scale(1.3);
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.65);
+    }
   }
 `,Qe=Se.i`
   font-size: 3em;
